6f753124a21022b0f604a2b61d118a4575a58388
errz99 <errz99@gmail.com>
2020-10-02 17:09:11 +0200

    added gtk dials and example gtkDials

---
+++ examples/gtkDials.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"strconv"
+	"strings"
+
+	"github.com/errz99/mag/gtk/dials"
+	"github.com/gotk3/gotk3/gtk"
+)
+
+func main() {
+
+	gtk.Init(nil)
+	mainWindow()
+	gtk.Main()
+
+}
+
+func mainWindow() {
+	cL := "|    "
+	cR := "    |"
+
+	window, _ := gtk.WindowNew(gtk.WINDOW_TOPLEVEL)
+	window.SetTitle("Dialogs")
+	window.Connect("destroy", func() {
+		gtk.MainQuit()
+	})
+
+	vbox, _ := gtk.BoxNew(gtk.ORIENTATION_VERTICAL, 0)
+	vbox.SetBorderWidth(5)
+	vbox.SetCanFocus(false)
+	window.Add(vbox)
+
+	head, _ := gtk.LabelNew("")
+	head.SetMarkup("<span color=\"green\"><b>Select a Dialog</b></span>")
+	head.SetMarginTop(8)
+	head.SetMarginBottom(8)
+	vbox.Add(head)
+
+	buttonTexts := []string{
+		"Info, Warning, Error",
+		"Question Dialog",
+		"Two Labels",
+		"Choose A File",
+		"One Entry",
+		"Multi Entries",
+		"Show Edit Text",
+		"Exit"}
+
+	for i, text := range buttonTexts {
+		button, _ := gtk.ButtonNewWithLabel(text)
+		button.SetName(strconv.Itoa(i + 1))
+		button.SetRelief(gtk.RELIEF_HALF)
+		button.SetSizeRequest(240, 0)
+		vbox.Add(button)
+
+		button.Connect("focus-in-event", func() {
+			label, _ := button.GetLabel()
+			button.SetLabel(cL + label + cR)
+		})
+
+		button.Connect("focus-out-event", func() {
+			label, _ := button.GetLabel()
+			label = strings.TrimLeft(label, cL)
+			label = strings.TrimRight(label, cR)
+			button.SetLabel(label)
+		})
+
+		button.Connect("clicked", func() {
+			name, _ := button.GetName()
+
+			switch name {
+			case "1":
+				dials.Message(0, 0, "Info", "Message Dialog",
+					"Info | Warning | Error Message", window)
+
+			case "2":
+				dials.Question(0, 0, "Question Dialog", "Wanna continue?", window)
+
+			case "3":
+				dials.TwoLabels("Title", "Text One", "Text Two", window)
+
+			case "4":
+				resp, file := dials.ChooseAFile("Select a File", window)
+				if resp == -5 {
+					dials.Message(0, 0, "Info", "File Selected", file, window)
+				}
+
+			case "5":
+				resp, text := dials.OneEntry("Title", "Head", "Label", "Entry text", true, window)
+				if resp == -5 {
+					dials.Message(0, 0, "Info", "Entry text", text, window)
+				}
+
+			case "6":
+				resp, texts := dials.MultiEntries("Title", "Head",
+					[]string{"Label1", "Label2"},
+					[]string{"Entry1 text", "Entry2 text"},
+					[]bool{true, true}, window)
+				if resp == -5 {
+					dials.Message(0, 0, "Info", "Entries texts", texts[0]+"\n...", window)
+				}
+
+			case "7":
+				editable := "This text can be edited\nand returned."
+				resp, edited := dials.ShowEditText("Title", "Head",
+					"file text here\n and here", editable, window)
+				if resp == -5 {
+					dials.Message(0, 0, "Info", "Edited Text", edited, window)
+				}
+
+			case "8":
+				gtk.MainQuit()
+
+			default:
+			}
+		})
+	}
+
+	window.ShowAll()
+}
---
+++ gtk/dials/dials.go
@@ -0,0 +1,206 @@
+package dials
+
+import (
+	"os"
+
+	"github.com/gotk3/gotk3/gtk"
+)
+
+// Message shows an Info, Warning or Error message
+func Message(w, h int, class, title, msg string, win gtk.IWindow) {
+	var dialog *gtk.MessageDialog
+	switch class {
+	case "Info":
+		dialog = gtk.MessageDialogNew(win, 1, gtk.MESSAGE_INFO, gtk.BUTTONS_CLOSE, msg)
+	case "Warning":
+		dialog = gtk.MessageDialogNew(win, 1, gtk.MESSAGE_WARNING, gtk.BUTTONS_CLOSE, msg)
+	case "Error":
+		dialog = gtk.MessageDialogNew(win, 1, gtk.MESSAGE_ERROR, gtk.BUTTONS_CLOSE, msg)
+	default:
+		dialog = gtk.MessageDialogNew(win, 1, gtk.MESSAGE_OTHER, gtk.BUTTONS_CLOSE, msg)
+	}
+
+	dialog.SetTitle(title)
+	dialog.SetSizeRequest(w, h)
+	dialog.Run()
+	dialog.Destroy()
+}
+
+// Question shows a dialog allowing to cancel or accept a question
+func Question(w, h int, title, msg string, win gtk.IWindow) gtk.ResponseType {
+	dialog := gtk.MessageDialogNew(win, 1, gtk.MESSAGE_QUESTION, gtk.BUTTONS_OK_CANCEL, msg)
+	dialog.SetTitle(title)
+	dialog.SetSizeRequest(w, h)
+	answer := dialog.Run()
+	dialog.Destroy()
+
+	return answer
+}
+
+// OneEntry shows a dialog with an entry widget
+func OneEntry(title, head, entryLabel, entryText string,
+	visible bool, win *gtk.Window) (gtk.ResponseType, string) {
+
+	dial, _ := gtk.DialogNew()
+	dial.SetTransientFor(win)
+	dial.SetTitle(title)
+
+	content, _ := dial.GetContentArea()
+	content.SetSpacing(4)
+	label, _ := gtk.LabelNew(head)
+
+	hbox, _ := gtk.BoxNew(gtk.ORIENTATION_HORIZONTAL, 4)
+	name, _ := gtk.LabelNew(entryLabel)
+	entry, _ := gtk.EntryNew()
+	entry.SetText(entryText)
+	entry.SetWidthChars(32)
+	entry.SetVisibility(visible)
+	hbox.PackStart(name, false, true, 4)
+	hbox.PackStart(entry, true, true, 4)
+
+	content.PackStart(label, false, true, 4)
+	content.PackStart(hbox, false, true, 4)
+
+	dial.AddButton("Cancel", -6)
+	dial.AddButton("OK", -5)
+	dial.SetDefaultResponse(-5)
+	dial.ShowAll()
+
+	answer := dial.Run()
+	entrName, _ := entry.GetText()
+
+	dial.Destroy()
+	return answer, entrName
+}
+
+// MultiEntries shows a dialog with multiple entries
+func MultiEntries(title, head string, entryLabels, entryTexts []string,
+	visibles []bool, win *gtk.Window) (gtk.ResponseType, []string) {
+
+	dial, _ := gtk.DialogNew()
+	dial.SetTransientFor(win)
+	dial.SetTitle(title)
+
+	content, _ := dial.GetContentArea()
+	content.SetSpacing(6)
+
+	label, _ := gtk.LabelNew(head)
+	content.PackStart(label, false, true, 4)
+	var entries []*gtk.Entry
+
+	grid, _ := gtk.GridNew()
+	grid.SetHAlign(gtk.ALIGN_FILL)
+	grid.SetRowSpacing(6)
+	grid.SetColumnSpacing(6)
+	grid.SetMarginStart(6)
+	grid.SetMarginEnd(6)
+	content.Add(grid)
+
+	for i, entryText := range entryTexts {
+		name, _ := gtk.LabelNew(entryLabels[i])
+		name.SetHAlign(gtk.ALIGN_END)
+		entry, _ := gtk.EntryNew()
+		entry.SetText(entryText)
+		entry.SetWidthChars(32)
+		entry.SetVisibility(visibles[i])
+		entries = append(entries, entry)
+
+		grid.Attach(name, 0, i, 1, 1)
+		grid.Attach(entry, 1, i, 1, 1)
+	}
+
+	dial.AddButton("Cancel", -6)
+	dial.AddButton("OK", -5)
+	dial.SetDefaultResponse(-5)
+	dial.ShowAll()
+
+	answer := dial.Run()
+	var entryNames []string
+
+	for _, entry := range entries {
+		text, _ := entry.GetText()
+		entryNames = append(entryNames, text)
+	}
+
+	dial.Destroy()
+	return answer, entryNames
+}
+
+// ChooseAFile returns a file name (with path included)
+func ChooseAFile(head string, win gtk.IWindow) (gtk.ResponseType, string) {
+	fchooser, _ := gtk.FileChooserDialogNewWith2Buttons(head, win,
+		gtk.FILE_CHOOSER_ACTION_OPEN, "Open", -5, "Cancel", -6)
+	fchooser.SetCurrentFolder(os.Getenv("HOME") + "/")
+	resp := fchooser.Run()
+	fname := fchooser.GetFilename()
+	fchooser.Destroy()
+	return resp, fname
+}
+
+// ShowEditText shows a text in a TextView and returns its edition
+func ShowEditText(title, head, file, text string, parent gtk.IWindow) (gtk.ResponseType, string) {
+	dial, _ := gtk.DialogNew()
+	dial.SetTransientFor(parent)
+	dial.SetTitle(title)
+	dial.SetDefaultSize(0, 0)
+
+	content, _ := dial.GetContentArea()
+	content.SetSpacing(4)
+	content.SetBorderWidth(8)
+
+	label, _ := gtk.LabelNew(head)
+	label.SetMarkup("<span foreground=\"red\" size=\"medium\">" + head + "</span>")
+	label.SetMarginBottom(4)
+	content.Add(label)
+
+	tag, _ := gtk.TextTagNew("tag1")
+	table, _ := gtk.TextTagTableNew()
+	table.Add(tag)
+	buffer, _ := gtk.TextBufferNew(table)
+	buffer.SetText(text)
+	tview, _ := gtk.TextViewNewWithBuffer(buffer)
+
+	tview.SetLeftMargin(4)
+	tview.SetRightMargin(4)
+	tview.SetWrapMode(gtk.WRAP_WORD)
+	content.Add(tview)
+
+	dial.AddButton("Cancel", -6)
+	dial.AddButton("OK", -5)
+	dial.SetDefaultResponse(-6)
+	dial.ShowAll()
+
+	answer := dial.Run()
+	a, b := buffer.GetBounds()
+	text, _ = buffer.GetText(a, b, false)
+
+	dial.Destroy()
+	return answer, text
+}
+
+// TwoLabels shows two texts with different colors
+func TwoLabels(title, text1, text2 string, parent gtk.IWindow) {
+	dial, _ := gtk.DialogNew()
+	dial.SetTransientFor(parent)
+	dial.SetTitle(title)
+
+	content, _ := dial.GetContentArea()
+	content.SetSpacing(6)
+	content.SetMarginTop(6)
+	content.SetMarginBottom(6)
+	content.SetMarginStart(12)
+	content.SetMarginEnd(12)
+
+	label1, _ := gtk.LabelNew(text1)
+	label1.SetMarkup("<span color=\"green\"><b>" + text1 + "</b></span>")
+	label2, _ := gtk.LabelNew(text2)
+	label2.SetMarkup("<span><tt>" + text2 + "</tt></span>")
+	content.Add(label1)
+	content.Add(label2)
+
+	dial.AddButton("OK", -5)
+	dial.ShowAll()
+
+	dial.Run()
+	dial.Destroy()
+}
