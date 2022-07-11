671d67b6ac2cea9dfec155b1e4f017ac32a43844
errz99 <errz99@gmail.com>
2022-07-11 22:44:24 +0200

    gtk/menu: new func to use an AccelGroup when creating menus

---
+++ go.mod
@@ -0,0 +1,30 @@
+module mag
+
+go 1.18
+
+require (
+	fyne.io/fyne v1.4.3
+	github.com/errz99/mag v0.0.0-20210214164107-222da76220f1
+	github.com/gotk3/gotk3 v0.6.1
+	github.com/therecipe/qt v0.0.0-20200904063919-c0c124a5770d
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/fsnotify/fsnotify v1.4.9 // indirect
+	github.com/fyne-io/mobile v0.1.2 // indirect
+	github.com/go-gl/gl v0.0.0-20190320180904-bf2b1f2f34d7 // indirect
+	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20200625191551-73d3c3675aa3 // indirect
+	github.com/godbus/dbus/v5 v5.0.3 // indirect
+	github.com/goki/freetype v0.0.0-20181231101311-fa8a33aabaff // indirect
+	github.com/gopherjs/gopherjs v0.0.0-20190411002643-bd77b112433e // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/srwiley/oksvg v0.0.0-20200311192757-870daf9aa564 // indirect
+	github.com/srwiley/rasterx v0.0.0-20200120212402-85cb7272f5e9 // indirect
+	github.com/stretchr/testify v1.5.1 // indirect
+	golang.org/x/image v0.0.0-20200430140353-33d19683fad8 // indirect
+	golang.org/x/net v0.0.0-20200324143707-d3edc9973b7e // indirect
+	golang.org/x/sys v0.0.0-20200720211630-cb9d2d5c5666 // indirect
+	golang.org/x/text v0.3.2 // indirect
+	gopkg.in/yaml.v2 v2.2.8 // indirect
+)
--- gtk/menu/menu.go
+++ gtk/menu/menu.go
@@ -4,19 +4,21 @@ import (
 	"github.com/gotk3/gotk3/gtk"
 )
 
+var NoStr = "-"
+
 // FillWithItems creates menu items from a slice of names and icons.
 func FillWithItems(menu *gtk.Menu, items []*gtk.MenuItem, names []string, icons []string) {
 	n := 0
 
 	for i, name := range names {
-		if name == "-" {
+		if name == NoStr {
 			sep, _ := gtk.SeparatorMenuItemNew()
 			sep.Show()
 			menu.Append(sep)
 			continue
 
 		} else {
-			if icons != nil && icons[i] != "-" {
+			if icons != nil && icons[i] != NoStr {
 				lbl, _ := gtk.LabelNew(name)
 				icon, _ := gtk.ImageNew()
 				icon.SetSizeRequest(16, 16)
@@ -36,6 +38,42 @@ func FillWithItems(menu *gtk.Menu, items []*gtk.MenuItem, names []string, icons 
 				menu.Append(items[n])
 			}
 
+			n++
+		}
+	}
+}
+
+// FillWithItemsAndAccels creates menu items from a slice of names and icons
+// including AccelGroup.
+func FillWithItemsAndAccels(
+	menu *gtk.Menu, items []*gtk.MenuItem,
+	names []string, accels []string, agroup *gtk.AccelGroup) {
+
+	n := 0
+	for _, name := range names {
+		if name == NoStr {
+			sep, _ := gtk.SeparatorMenuItemNew()
+			sep.Show()
+			menu.Append(sep)
+			continue
+		} else {
+			items[n].SetLabel(name)
+			items[n].Show()
+
+			if accels[n] != NoStr {
+				key, modifier := gtk.AcceleratorParse(accels[n])
+				if agroup != nil {
+					items[n].AddAccelerator(
+						"activate",
+						agroup,
+						key,
+						modifier,
+						gtk.ACCEL_VISIBLE,
+					)
+				}
+			}
+
+			menu.Append(items[n])
 			n++
 		}
 	}
